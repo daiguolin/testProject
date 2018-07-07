43c20894b4342d1e9a11504ff516a814d1bf4c5d
daiguolin <1396885088@qq.com>
2018-07-07 11:23:46 +0800

    修改了

---
+++ 未命名1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#inlclude<iomanip> 
+#include<math.h>
+using namespace std;
+int main()
+{
+	double a,b,c,i,j;
+	a=1/4.0;
+	b=cos(30*(180/3.14));
+	c=sin(30*(3.141592/180));
+	i=tan(50);
+	cout<<setiosflag(ios::fixed)<<setprecision(3)<<a<<endl;
+ } 
